57cd2480be2481826e546bae6b18a49ef12757b9
Jessica <jmckinney48045@gmail.com>
2020-03-08 22:37:08 -0400

    First commit Deliverables2 Unit 1

---
+++ Deliverables2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+namespace Deliverables2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("What is your message?: ");
+
+                String input = System.Console.ReadLine();
+                String inputUp = input.ToUpper();
+
+                //converting string to char[]
+                char[] char_arr = inputUp.ToCharArray();
+                foreach (char item in char_arr)
+                {
+                    Console.Write(item + " ");
+                }
+
+                foreach (char c in inputUp)
+
+                {
+                    Console.WriteLine((int)c - 64);
+                }
+                
+                Console.WriteLine("str: " + inputUp);
+
+                //printing char[]
+                Console.WriteLine("char_arr...");
+                
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ Deliverables2/size_t.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deliverables2
+{
+    internal class size_t
+    {
+        public static implicit operator size_t(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
